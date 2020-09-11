6b26d1170e21f5d1fa48eebce2069d3800246fc9
Gregor von Laszewski <laszewski@gmail.com>
2020-09-11 09:14:38 -0400

    test site

--- init.sh
+++ init.sh
@@ -2,23 +2,20 @@ rm -rf docs
 hugo new site docs
 touch docs/.nojekyll	
 cd docs
-hugo new blog/welcome.md
+#hugo new blog/welcome.md
 # wget https://github.com/denmch/ananke-mod/blob/master/exampleSite/config.toml
-git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
-echo 'theme = "ananke"' >> config.toml
-hugo serve
+#git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
+#echo 'theme = "ananke"' >> config.toml
+#hugo serve
 
 #git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
 
 
 
-ls
- 1224  ls  config.toml
- 1225  e  config.toml
- 1226  hugo serve
- 1227  hugo mod init clusterpi.github.io
- 1228  ls
- 1229  hugo serve
- 1230  hugo serve
- 1231  ls
- 1232  cd ..
+#ls
+hugo mod init clusterpi.github.io
+# 1228  ls
+# 1229  hugo serve
+# 1230  hugo serve
+# 1231  ls
+# 1232  cd ..
